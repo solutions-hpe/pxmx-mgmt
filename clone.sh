e4ad0d85e75104dcdf1cf7444e21a4050952bf83
Lance Bockenstedt <lrb@hpe.com>
2025-01-26 09:33:18 -0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#--------------------------------------------------------------------------------------------------------------
+#Variable Setup - Reading arguments passed to script
 command="$1"
 start_vmid="$2"
 end_vmid="$3"
@@ -8,29 +10,65 @@ sleep_time="$6"
 pool_id="$7"
 vm_name="$8"
 tpl_id="$9"
+#--------------------------------------------------------------------------------------------------------------
+#Re-Create VMS - Delete, re-clone from template
+#Delete and Re-Create the VMs
+if [ $commnad == "re-create" ] 
+  then
+    for (( i=$start_vmid ; i <= $end_vmid ; i++ ))
+    do
+     echo stopping $i
+     qm stop $i
+     echo deleting VM $i
+     qm destroy $i --skiplock --purge --destroy-unreferenced-disks
+     echo Cloning $tpl_id to $i
+     qm clone $tpl_id $i --name $vm_name-$i --pool $pool_id
+     echo Configuring $i Network
+     qm set $i --net0 model=virtio,bridge=$bridge_id,firewall=1,tag=$vlan_id
+     echo Starting $i
+     qm start $i
+     sleep $sleep_time
+    done
+#Configuring VM Clones
+    for (( i=$start_vmid ; i <= $end_vmid ; i++ ))
+    do
+     echo Renaming $i to $vm_name-$i
+     qm guest exec $i hostname $vm_name-$i
+     qm guest exec $1 rm /usr/local/scripts/vhcached.txt
+     qm guest exec $1 /usr/sbin/vhclientx86_64 -t "STOP USING ALL LOCAL"
+     sleep $sleep_time
+     echo Rebooting $i
+     qm guest exec $i reboot now
+     sleep $sleep_time
+    done
+fi
+#--------------------------------------------------------------------------------------------------------------
+#Delete Option - delete all the VMs and cleanup disks
+if [ $commnad == "delete" ]
+  then
   for (( i=$start_vmid ; i <= $end_vmid ; i++ ))
-  do
-   echo stopping $i
-   qm stop $i
-   echo deleting VM $i
-   qm destroy $i --skiplock --purge --destroy-unreferenced-disks
-   echo Cloning $tpl_id to $i
-   qm clone $tpl_id $i --name $vm_name-$i --pool $pool_id
-   echo Configuring $i Network
-   qm set $i --net0 model=virtio,bridge=$bridge_id,firewall=1,tag=$vlan_id
-   echo Starting $i
-   qm start $i
-   sleep $sleep_time
-  done
+    do
+     echo stopping $i
+     qm stop $i
+     echo deleting VM $i
+     qm destroy $i --skiplock --purge --destroy-unreferenced-disks
+    done
+fi
+#--------------------------------------------------------------------------------------------------------------
+#Reconfigure VMs - reset the network interface settings, hostname, and clear out VH caches
+if [ $commnad == "config" ]
+  then
+  for (( i=$start_vmid ; i <= $end_vmid ; i++ ))
+    do
+     echo Configuring $i Network
+     qm set $i --net0 model=virtio,bridge=$bridge_id,firewall=1,tag=$vlan_id
+     echo Renaming $i to $vm_name-$i
+     qm guest exec $i hostname $vm_name-$i
+     qm guest exec $1 rm /usr/local/scripts/vhcached.txt
+     qm guest exec $1 /usr/sbin/vhclientx86_64 -t "STOP USING ALL LOCAL"
+     echo Rebooting $i
+     qm guest exec $i reboot now
+    done
+fi
+#--------------------------------------------------------------------------------------------------------------
 
-  for (( i=$start_vmid ; i <= $end_vmid ; i++ ))
-  do
-   echo Renaming $i to $vm_name-$i
-   qm guest exec $i hostname $vm_name-$i
-   qm guest exec $1 rm /usr/local/scripts/vhcached.txt
-   qm guest exec $1 /usr/sbin/vhclientx86_64 -t "STOP USING ALL LOCAL"
-   sleep $sleep_time
-   echo Rebooting $i
-   qm guest exec $i reboot now
-   sleep $sleep_time
-done
